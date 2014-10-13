f148ee74c316f6bb6af1eaca1b7605e46195501c
Liam Morley <liam@carpeliam.com>
2014-10-13 15:44:54 -0400

    get past SSL cert issues

---
+++ app/controllers/authentications_controller.rb
@@ -0,0 +1,63 @@
+class AuthenticationsController < ActionController::Base
+  # layout "application"
+  # def index
+  #   @authentications = current_user.authentications if current_user
+  # end
+
+  
+  def create
+    omniauth = request.env['omniauth.auth']
+
+    # FIXME what error handling needs to go here?
+    if omniauth[:provider] == :idpp
+      token = omniauth[:credentials][:token]
+      raise token
+    end
+
+    
+
+
+    # authentication = Authentication.where(provider: omniauth['provider'], uid: omniauth['uid']).first
+
+    # if authentication
+    #   AuditLog.create(requester_info: authentication.user, event: "user_auth", description: "successful sign in")
+    #   flash[:notice] = "Signed in successfully."
+    #   sign_in_and_redirect(:user, authentication.user)
+    # elsif current_user
+    #   current_user.authentications.create!(:provider => omniauth['provider'], :uid => omniauth['uid'])
+    #   AuditLog.create(requester_info: current_user.email, event: "user_auth2", description: "successful sign in")
+    #   flash[:notice] = "Authentication successful."
+    #   redirect_to authentications_url
+    # else
+    #   user = User.new
+    #   user.apply_omniauth(omniauth)
+    #   if user.save
+    #     user.authentications[0].save
+    #     AuditLog.create(requester_info: user.email, event: "user_auth3", description: "successful account create and sign in")
+    #     flash[:notice] = "Signed in successfully."
+    #     sign_in_and_redirect(:user, user)
+    #   else
+    #     session[:omniauth] = omniauth.except('extra')
+    #     redirect_to new_user_registration_url
+    #   end
+    # end
+  end
+
+  # def destroy
+  #   @authentication = current_user.authentications.find(params[:id])
+  #   @authentication.destroy
+  #   flash[:notice] = "Successfully destroyed authentication."
+  #   redirect_to authentications_url
+  # end
+
+
+  protected
+
+  # This is necessary since Rails 3.0.4
+  # See https://github.com/intridea/omniauth/issues/185
+  # and http://www.arailsdemo.com/posts/44
+  # def handle_unverified_request
+  #   true
+  # end
+  
+end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,18 @@
+# Use the system cert store
+Rack::OAuth2.http_config do |http_client|
+  http_client.ssl_config.clear_cert_store
+  http_client.ssl_config.cert_store.set_default_paths
+end
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
+  provider :openid_connect, {
+    name: :idpp,
+    discovery: true,
+    client_options: {
+      host: "idp-p.mitre.org",
+      identifier: '',
+      secret: ''
+    }
+  }
 end
