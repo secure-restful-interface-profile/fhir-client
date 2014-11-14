d9349065423181b1dde101aefc96ab54e11ae7d3
Dave Hill <dwhill@mitre.org>
2014-11-14 07:16:08 -0500

    Fixed JWKS endpoint.

--- app/controllers/jwk_controller.rb
+++ app/controllers/jwk_controller.rb
@@ -5,14 +5,11 @@ class JwkController < ApplicationController
   def jwk
     Rails.logger.debug "========== Begin JWKS endpoint =========="
     jwk = JSON::JWK.new(Application.public_key)
-
-    jwks = Hash.new
-    jwks["keys"] = JSON::JWK::Set.new(jwk)
+    jwks = JSON::JWK::Set.new(jwk)
 
     Rails.logger.debug "------ JWKS = #{jwks} ------"
     Rails.logger.debug "========== End JWK endpoint =========="
-
-    respond_with jwks
+    render json: jwks
   end
 
 end
