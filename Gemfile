849ce3ab88ae0dcdd541913f3ced5daf78a9eae7
Dave Hill <dwhill@mitre.org>
2014-11-17 08:32:25 -0500

    Added protected attributes gem.

--- Gemfile
+++ Gemfile
@@ -35,6 +35,7 @@ gem 'spring',        group: :development
 
 gem 'less-rails'
 gem 'twitter-bootstrap-rails'
+gem 'protected_attributes'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
 
+  attr_accessible   :provider, :uid, :name
+  
+  #-------------------------------------------------------------------------------
+ 
   def self.from_omniauth(auth)
     where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
   end
