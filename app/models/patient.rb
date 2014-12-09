a65dadeb25b1f687ad7fa0322a1fbc94961e940b
Dave Hill <dwhill@mitre.org>
2014-12-08 21:38:42 -0500

    Made patient fields consistent in format with other model fields.

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,20 +1,14 @@
 class Patient
 
-  def name
-    [ patient["familyName"], patient["givenName"] ].join(', ')
-  end
-
-  #-------------------------------------------------------------------------------
-
   def self.parse_from_fhir(fhir)
     patients = []
 
     fhir["entry"].map do |resource|
       patient = Hash.new
 
-      patient["familyName"]   = resource["content"]["name"].first["family"]
-      patient["givenName"]    = resource["content"]["name"].first["given"]
-      patient["birthDate"]    = Date.parse(resource["content"]["birthDate"])
+      patient["family_name"]   = resource["content"]["name"].first["family"]
+      patient["given_name"]    = resource["content"]["name"].first["given"]
+      patient["birth_date"]    = Date.parse(resource["content"]["birthDate"])
       patient["id"]           = resource["id"]
 
       patients << patient
